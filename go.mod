fce415fdcf76a852a57f79840490447a31573cab
我如天翼 <wrtyis@outlook.com>
2022-04-17 16:59:16 +0800

    重构

--- go.mod
+++ go.mod
@@ -1,26 +1,35 @@
 module sensor
 
-go 1.14
+go 1.17
 
 require (
 	github.com/d2r2/go-bh1750 v0.0.0-20181222061755-1195122364ab
 	github.com/d2r2/go-bsbmp v0.0.0-20190515110334-3b4b3aea8375
 	github.com/d2r2/go-i2c v0.0.0-20191123181816-73a8a799d6bc
-	github.com/d2r2/go-logger v0.0.0-20181221090742-9998a510495e
+	github.com/d2r2/go-logger v0.0.0-20210606094344-60e9d1233e22
 	github.com/gin-gonic/gin v1.7.1
-	github.com/go-delve/delve v1.6.1 // indirect
-	github.com/mattn/go-colorable v0.1.8 // indirect
-	github.com/mattn/go-runewidth v0.0.12 // indirect
-	github.com/peterh/liner v1.2.1 // indirect
-	github.com/rivo/uniseg v0.2.0 // indirect
-	github.com/robfig/cron v1.2.0
-	github.com/russross/blackfriday v1.6.0 // indirect
-	github.com/russross/blackfriday/v2 v2.1.0 // indirect
-	github.com/sirupsen/logrus v1.8.1 // indirect
-	github.com/spf13/cobra v1.1.3 // indirect
-	go.starlark.net v0.0.0-20210511153848-cca21e7857d4 // indirect
-	golang.org/x/arch v0.0.0-20210502124803-cbf565b21d1e // indirect
+	github.com/used255/aht20-go v0.1.0
+)
+
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/gin-contrib/sse v0.1.0 // indirect
+	github.com/go-playground/locales v0.13.0 // indirect
+	github.com/go-playground/universal-translator v0.17.0 // indirect
+	github.com/go-playground/validator/v10 v10.4.1 // indirect
+	github.com/golang/protobuf v1.4.1 // indirect
+	github.com/google/go-cmp v0.5.1 // indirect
+	github.com/json-iterator/go v1.1.9 // indirect
+	github.com/kr/pretty v0.1.0 // indirect
+	github.com/leodido/go-urn v1.2.0 // indirect
+	github.com/mattn/go-isatty v0.0.12 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.1 // indirect
+	github.com/ugorji/go/codec v1.1.7 // indirect
+	golang.org/x/crypto v0.0.0-20200622213623-75b288015ac9 // indirect
 	golang.org/x/sys v0.0.0-20210521203332-0cec03c779c1 // indirect
-	gorm.io/driver/sqlite v1.1.4
-	gorm.io/gorm v1.21.8
+	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
+	google.golang.org/protobuf v1.25.0 // indirect
+	gopkg.in/check.v1 v1.0.0-20180628173108-788fd7840127 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,98 +1,92 @@
 package main
 
 import (
-	"bytes"
-	"io"
+	"flag"
 	"log"
-	"net/http"
-	"os"
-	"time"
 
 	"github.com/d2r2/go-bh1750"
 	"github.com/d2r2/go-bsbmp"
 	"github.com/d2r2/go-i2c"
 	logger "github.com/d2r2/go-logger"
 	"github.com/gin-gonic/gin"
-	"github.com/robfig/cron"
-	"gorm.io/driver/sqlite"
-	"gorm.io/gorm"
-)
-
-type sensor_sampling_data struct {
-	gorm.Model
-	Temperature       float32 //`gorm:"default:null"`
-	AHT20_Temperature float32 //`gorm:"default:null"`
-	Humidity          float32 //`gorm:"default:null"`
-	Pressure          float32 //`gorm:"default:null"`
-	Altitude          float32 //`gorm:"default:null"`
-	Illuminance       float32 //`gorm:"default:null"`
-	Time_Consuming    float32 //`gorm:"default:null"`
-	bmp280_err        string  `gorm:"default:null"`
-	bh1750_err        string  `gorm:"default:null"`
-	aht20_err         string  `gorm:"default:null"`
-}
-
-type caiyun_sampling_date struct {
-	gorm.Model
-	Response       string  `gorm:"default:null"`
-	Time_Consuming float32 `gorm:"default:null"`
-	Err            string  `gorm:"default:null"`
-}
-
-var db *gorm.DB
-var err error
-var logg *log.Logger
-
-const (
-	//I = "\033[0m\n\033[1;32m[I]"
-	I string = ""
-	W string = "\033[0m\n\032[1;32m[W]"
-	F string = "\033[0m\n\031[1;32m[F]"
+	aht20 "github.com/used255/aht20-go"
 )
 
 func main() {
-	logg = log.New(io.MultiWriter(os.Stdout), I, log.Ldate|log.Ltime|log.Lshortfile)
-	logg.SetPrefix(I)
-	logg.Println("欢迎😺")
-	defer logg.Println("再见👋")
-	db, err = gorm.Open(sqlite.Open("sensor.db"), &gorm.Config{})
-	if err != nil {
-		logg.SetPrefix(F)
-		logg.Fatal("failed to connect database")
+	bindFlagPtr := flag.String("bind", ":8080", "bind address")
+	disableGinModeFlagPtr := flag.Bool("disable-gin-debug-mode", false, "gin.ReleaseMode")
+	flag.Parse()
+	if *disableGinModeFlagPtr {
+		gin.SetMode(gin.ReleaseMode)
 	}
-	db.AutoMigrate(&sensor_sampling_data{}, &caiyun_sampling_date{})
-
-	sensor_sampling_task := cron.New()
-	err = sensor_sampling_task.AddFunc("0 0/1 * * * ?", sensor_sampling_work)
-	if err != nil {
-		logg.SetPrefix(F)
-		logg.Fatalln(err)
-	}
-	sensor_sampling_work()
-	sensor_sampling_task.Start()
-	defer sensor_sampling_task.Stop()
-
-	caiyun_sampling_task := cron.New()
-	err = caiyun_sampling_task.AddFunc("0 0/15 * * * ?", caiyun_sampling_work)
-	if err != nil {
-		logg.SetPrefix(F)
-		logg.Fatalln(err)
-	}
-	caiyun_sampling()
-	caiyun_sampling_task.Start()
-	defer caiyun_sampling_task.Stop()
-
-	gin.SetMode(gin.ReleaseMode)
+	log.Println("Welcome 🐱‍🏍")
 	r := gin.Default()
-	r.GET("/ping", ping)
+	//r.SetTrustedProxies([]string{"192.168.0.0/24", "172.16.0.0/12", "10.0.0.0/8"})
 	api := r.Group("/api/v1")
 	{
-		api.GET("/environmental_sampling_data", Get_environmental_sampling_data)
-		api.GET("/current_environmental_sampling_data", Get_current_environmental_sampling_data)
+		api.GET("/ping",
+			func(c *gin.Context) {
+				c.JSON(200, gin.H{
+					"message": "pong",
+				})
+			})
+		api.GET("/sampling", sampling)
 	}
-	r.Run()
+	r.Run(*bindFlagPtr)
+}
 
-	select {}
+func sampling(c *gin.Context) {
+	c.JSON(200, sampling_json())
+}
+
+func sampling_json() gin.H {
+	return gin.H{
+		"aht20":  aht20_sampling_json(),
+		"bh1750": bh1750_sampling_json(),
+		"bmp280": bmp280_sampling_json(),
+	}
+}
+
+func aht20_sampling_json() gin.H {
+	temperature, humidity, err := aht20_sampling()
+	j := gin.H{"temperature": temperature, "humidity": humidity}
+	if err != nil {
+		j["error"] = err.Error()
+	}
+	return j
+}
+
+func bh1750_sampling_json() gin.H {
+	amb, err := bh1750_sampling()
+	j := gin.H{"ambient": amb}
+	if err != nil {
+		j["error"] = err.Error()
+	}
+	return j
+}
+
+func bmp280_sampling_json() gin.H {
+	temperature, altitude, pressure, err := bmp280_sampling()
+	j := gin.H{"temperature": temperature, "altitude": altitude, "pressure": pressure}
+	if err != nil {
+		j["error"] = err.Error()
+	}
+	return j
+}
+
+func aht20_sampling() (float32, float32, error) {
+	logger.ChangePackageLogLevel("i2c", logger.InfoLevel)
+	logger.ChangePackageLogLevel("aht20", logger.InfoLevel)
+	bus, err := i2c.NewI2C(0x38, 1)
+	if err != nil {
+		return 0, 0, err
+	}
+	aht20 := aht20.AHT20New(bus)
+	err = aht20.ReadWithRetry(3)
+	if err != nil {
+		return 0, 0, err
+	}
+	return aht20.Celsius(), aht20.RelHumidity(), nil
 }
 
 func bh1750_sampling() (uint16, error) {
@@ -145,100 +139,4 @@ func bmp280_sampling() (float32, float32, float32, error) {
 	}
 
 	return temperature, altitude, pressure, nil
-}
-
-func aht20_sampling() (float32, float32, error) {
-	logger.ChangePackageLogLevel("i2c", logger.InfoLevel)
-	bus, err := i2c.NewI2C(0x38, 1)
-	if err != nil {
-		return 0, 0, err
-	}
-	aht20 := AHT20New(bus)
-	err = aht20.ReadWithRetry(3)
-	if err != nil {
-		return 0, 0, err
-	}
-	return aht20.Celsius(), aht20.RelHumidity(), nil
-}
-
-func sensor_sampling() *sensor_sampling_data {
-	t2 := func(err error) string {
-		if err != nil {
-			return err.Error()
-		}
-		return ""
-	}
-	t1 := time.Now()
-	temperature, altitude, pressure, bmp280_err := bmp280_sampling()
-	if bmp280_err != nil {
-		logg.SetPrefix(W)
-		logg.Println(bmp280_err)
-	}
-	illuminance, bh1750_err := bh1750_sampling()
-	if bh1750_err != nil {
-		logg.SetPrefix(W)
-		logg.Println(bh1750_err)
-	}
-	aht20_temperature, humidity, aht20_err := aht20_sampling()
-	if aht20_err != nil {
-		logg.SetPrefix(W)
-		logg.Println(aht20_err)
-	}
-	elapsed := float32(time.Since(t1).Microseconds()) / float32(1000000)
-	return &sensor_sampling_data{Temperature: temperature, AHT20_Temperature: aht20_temperature, Humidity: humidity, Pressure: pressure, Altitude: altitude / 10, Illuminance: float32(illuminance), Time_Consuming: elapsed, bmp280_err: t2(bmp280_err), bh1750_err: t2(bh1750_err), aht20_err: t2(aht20_err)}
-}
-
-func sensor_sampling_work() {
-	data := sensor_sampling()
-	logg.SetPrefix(string(I))
-	logg.Printf("Temperature = %v*C, AHT20-Temperature = %.2f*C, Humidity = %.2f%%, Pressure = %vPa, Altitude = %.2fm, Illuminance = %vlux, Time-consuming = %vs\n",
-		data.Temperature, data.AHT20_Temperature, data.Humidity, data.Pressure, data.Altitude/10, float32(data.Illuminance), data.Time_Consuming)
-	db.Create(data)
-}
-
-func caiyun_sampling() *caiyun_sampling_date {
-	t1 := time.Now()
-	url := "https://api.caiyunapp.com/v2.5/OsND6NQQTmhh2yde/117.559364,39.764/realtime.json"
-	response, err := http.Get(url)
-	if err != nil {
-		logg.SetPrefix(W)
-		logg.Println(err)
-		return &caiyun_sampling_date{Time_Consuming: float32(time.Since(t1).Microseconds()) * float32(1000000), Err: err.Error()}
-	}
-	buf := new(bytes.Buffer)
-	_, err = buf.ReadFrom(response.Body)
-	if err != nil {
-		logg.SetPrefix(W)
-		logg.Println(err)
-		return &caiyun_sampling_date{Time_Consuming: float32(time.Since(t1).Microseconds()) * float32(1000000), Err: err.Error()}
-	}
-	res := buf.String()
-	elapsed := float32(time.Since(t1).Microseconds()) / float32(1000000)
-	return &caiyun_sampling_date{Response: res, Time_Consuming: elapsed}
-}
-
-func caiyun_sampling_work() {
-	res := caiyun_sampling()
-	logg.SetPrefix(I)
-	logg.Println(res)
-	db.Create(res)
-}
-
-func ping(c *gin.Context) {
-	//c.String(http.StatusOK, "pong")
-	res := gin.H{"status": http.StatusOK, "time": time.Now().Format(time.RFC3339), "message": "pong"}
-	logg.SetPrefix(I)
-	logg.Println(res)
-	c.JSON(http.StatusOK, res)
-}
-
-func Get_environmental_sampling_data(c *gin.Context) {
-
-}
-
-func Get_current_environmental_sampling_data(c *gin.Context) {
-	res := gin.H{"status": http.StatusOK, "time": time.Now().Format(time.RFC3339), "sensor_sampling": sensor_sampling(), "caiyun_sampling": caiyun_sampling()}
-	logg.SetPrefix(I)
-	logg.Println(res)
-	c.JSON(http.StatusOK, res)
 }
